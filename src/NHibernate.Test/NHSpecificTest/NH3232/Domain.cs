b3833fa97c07e3ac201e9193e41381388bf9875b
Alexander I. Zaytsev <hazzik@gmail.com>
2014-01-17 17:04:36 +1300

    Add test for NH-3232

---
+++ src/NHibernate.Test/NHSpecificTest/NH3232/Domain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH3232
+{
+	public class Page
+	{
+		public virtual string CreatedBy { get; set; }
+		public virtual DateTime CreatedOn { get; set; }
+		public virtual int Id { get; set; }
+		public virtual bool IsLocked { get; set; }
+		public virtual string ModifiedBy { get; set; }
+		public virtual DateTime ModifiedOn { get; set; }
+		public virtual string Tags { get; set; }
+		public virtual string Title { get; set; }
+	}
+
+	public class PageContent
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string EditedBy { get; set; }
+		public virtual DateTime EditedOn { get; set; }
+		public virtual int VersionNumber { get; set; }
+		public virtual string Text { get; set; }
+		public virtual Page Page { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH3232/Fixture.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using NHibernate.Dialect;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3232
+{
+	[TestFixture]
+	public class Fixture :  BugTestCase
+	{
+		protected override bool AppliesTo(Dialect.Dialect dialect)
+		{
+			return dialect is MsSqlCe40Dialect;
+		}
+
+		protected override void OnSetUp()
+		{
+			const string user = "test";
+
+			using (var session = sessions.OpenSession())
+			using (session.BeginTransaction())
+			{
+				var page = new Page
+					{
+						Title = "Blah",
+						Tags = "tag1 tag2 tag3",
+						CreatedBy = user,
+						CreatedOn = DateTime.Now,
+						ModifiedOn = DateTime.Now,
+						ModifiedBy = user
+					};
+				session.Save(page);
+				session.Save(new PageContent
+					{
+						VersionNumber = 1,
+						Text = "lots of content",
+						EditedBy = user,
+						EditedOn = DateTime.Now,
+						Page = page
+					});
+
+				session.Transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = sessions.OpenSession())
+			using (session.BeginTransaction())
+			{
+				session.Delete("from System.Object");
+				session.Transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void AbleToDoPagingWithMsSqlCe40Dialect()
+		{
+			using (var session = OpenSession())
+			using (session.BeginTransaction())
+			{
+				var page = session.Query<Page>().First();
+				var query = session
+					.CreateQuery("FROM PageContent fetch all properties WHERE Page.Id=:Id AND VersionNumber=(SELECT max(VersionNumber) FROM PageContent WHERE Page.Id=:Id)")
+					.SetCacheable(true)
+					.SetInt32("Id", page.Id)
+					.SetMaxResults(1);
+
+				var content = query.UniqueResult<PageContent>();
+				Console.WriteLine(content.Text);
+				Console.Read();
+			}
+		}
+	}
+}
