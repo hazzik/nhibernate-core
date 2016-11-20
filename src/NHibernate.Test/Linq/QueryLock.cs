5a79dbc44432d9c022f275f2f3d7be0f14cad6d2
Oskar Berggren <oskar.berggren@gmail.com>
2016-11-20 17:43:25 +0100

    QueryLock: Add test case to verify that a change in lock mode will avoid reusing cached query.

--- src/NHibernate.Test/Linq/QueryLock.cs
+++ src/NHibernate.Test/Linq/QueryLock.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Transactions;
+using NHibernate.Dialect;
 using NHibernate.DomainModel.Northwind.Entities;
 using NHibernate.Exceptions;
 using NHibernate.Linq;
@@ -10,7 +11,6 @@ namespace NHibernate.Test.Linq
 {
 	public class QueryLock : LinqTestCase
 	{
-
 		[Test]
 		public void CanSetLockLinqQueries()
 		{
@@ -40,7 +40,6 @@ namespace NHibernate.Test.Linq
 			}
 		}
 
-
 		[Test]
 		public void CanLockBeforeSkipOnLinqOrderedPageQuery()
 		{
@@ -49,14 +48,13 @@ namespace NHibernate.Test.Linq
 				var result = (from e in db.Customers
 				              orderby e.CompanyName
 				              select e)
-					.SetLockMode(LockMode.Upgrade).Skip(5).Take(5).ToList();
+				             .SetLockMode(LockMode.Upgrade).Skip(5).Take(5).ToList();
 
 				Assert.That(result, Has.Count.EqualTo(5));
 				Assert.That(session.GetCurrentLockMode(result[0]), Is.EqualTo(LockMode.Upgrade));
 				AssertSeparateTransactionIsLockedOut(result[0].CustomerId);
 			}
 		}
-
 
 		private void AssertSeparateTransactionIsLockedOut(string customerId)
 		{
@@ -68,13 +66,48 @@ namespace NHibernate.Test.Linq
 				Assert.Throws<GenericADOException>(
 					() =>
 					{
-						var result2 = (from e in s2.Query<Customer>()
-									   where e.CustomerId == customerId
-									   select e).SetLockMode(LockMode.UpgradeNoWait)
-												.Timeout(5).ToList();
-						Assert.IsNotNull(result2);
-					}, "Expected an exception to indicate locking failure due to already locked.");
+						var result2 = (
+								from e in s2.Query<Customer>()
+								where e.CustomerId == customerId
+								select e
+							).SetLockMode(LockMode.UpgradeNoWait)
+							 .WithOptions(o => o.SetTimeout(5))
+							 .ToList();
+						Assert.That(result2, Is.Not.Null);
+					},
+					"Expected an exception to indicate locking failure due to already locked.");
 			}
+		}
+
+		[Test]
+		[Description("Verify that different lock modes are respected even if the query is otherwise exactly the same.")]
+		public void CanChangeLockModeForQuery()
+		{
+			// Limit to a few dialects where we know the "nowait" keyword is used to make life easier.
+			Assume.That(Dialect is MsSql2000Dialect || Dialect is Oracle8iDialect || Dialect is PostgreSQL81Dialect);
+
+			using (session.BeginTransaction())
+			{
+				var result = BuildQueryableAllCustomers(db.Customers, LockMode.Upgrade).ToList();
+				Assert.That(result, Has.Count.EqualTo(91));
+
+				using (var logSpy = new SqlLogSpy())
+				{
+					// Only difference in query is the lockmode - make sure it gets picked up.
+					var result2 = BuildQueryableAllCustomers(session.Query<Customer>(), LockMode.UpgradeNoWait)
+						.ToList();
+					Assert.That(result2, Has.Count.EqualTo(91));
+
+					Assert.That(logSpy.GetWholeLog().ToLower(), Does.Contain("nowait"));
+				}
+			}
+		}
+
+		private static IQueryable<Customer> BuildQueryableAllCustomers(
+			IQueryable<Customer> dbCustomers,
+			LockMode lockMode)
+		{
+			return (from e in dbCustomers select e).SetLockMode(lockMode).WithOptions(o => o.SetTimeout(5));
 		}
 	}
 }
