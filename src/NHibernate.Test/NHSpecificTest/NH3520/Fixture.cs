95ef0714c0f5e71d8690d60a93c45a6cc4190b3b
Alexander Zaytsev <hazzik@gmail.com>
2018-06-16 23:00:56 +1200

    Add test for NH-3520

---
+++ src/NHibernate.Test/NHSpecificTest/NH3520/Fixture.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3520
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from System.Object").ExecuteUpdate();
+
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void Should_retrieve_orderlines_when_getting_order()
+		{
+			using (var session = OpenSession())
+			{
+				using (var transaction = session.BeginTransaction())
+				{
+					var newOrder = Order.Create("Order products");
+					var orderLine = OrderLine.Create("Product 1");
+					newOrder.AddLine(orderLine);
+
+					Assert.That(newOrder.Lines.Count, Is.EqualTo(1));
+					session.Save(newOrder);
+					session.Refresh(newOrder);
+					Assert.That(newOrder.Lines.Count, Is.EqualTo(1));
+
+					transaction.Commit();
+				}
+
+				using (var transaction = session.BeginTransaction())
+				{
+					var actual = session.Query<Order>().Single(x => x.Name == "Order products");
+					actual.AddLine(OrderLine.Create("Product 2"));
+
+					var actualLinesBeforeSave = actual.Lines;
+					Assert.That(actualLinesBeforeSave.Count, Is.EqualTo(2));
+					session.Save(actual);
+					session.Refresh(actual);
+					Assert.That(ReferenceEquals(actual.Lines, actualLinesBeforeSave));
+					Assert.That(actual.Lines.Count, Is.EqualTo(2));
+
+					transaction.Commit();
+				}
+			}
+		}
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH3520/Models.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace NHibernate.Test.NHSpecificTest.NH3520
+{
+	public class Order
+	{
+		public virtual int ID { get; set; }
+		public virtual string Name { get; set; }
+		public virtual IList<OrderLine> Lines { get; set; }
+
+		public static Order Create(string name)
+		{
+			return new Order
+			{
+				Name = name,
+				Lines = new List<OrderLine>(),
+			};
+		}
+
+		public virtual Order AddLine(OrderLine orderLine)
+		{
+			orderLine.Order = this;
+			Lines.Add(orderLine);
+			return this;
+		}
+	}
+
+	public class OrderLine
+	{
+		public virtual int ID { get; set; }
+		public virtual string Name { get; set; }
+		public virtual Order Order { get; set; }
+
+		public static OrderLine Create(string name)
+		{
+			return new OrderLine
+			{
+				Name = name,
+			};
+		}
+	}
+}
