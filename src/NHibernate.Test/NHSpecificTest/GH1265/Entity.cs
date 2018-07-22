cc4a7c069cbc5a0f12e9b41e34aaadbca9ef1031
Alexander Zaytsev <hazzik@gmail.com>
2018-07-22 23:43:43 +1200

    Add test for #1265

---
+++ src/NHibernate.Test/NHSpecificTest/GH1265/Entity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.GH1265
+{
+	class PersonChange : IEquatable<PersonChange>
+	{
+		public virtual int? ChangeId { get; set; }
+		public virtual int? ChangeSystemId { get; set; }
+		public virtual Person Person { get; set; }
+		
+		public virtual bool IsExportable { get; set; }
+
+		public virtual bool Equals(PersonChange other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return ChangeId == other.ChangeId &&
+			       ChangeSystemId == other.ChangeSystemId &&
+			       Person?.Id == other.Person?.Id;
+		}
+
+		public  override bool Equals(object obj)
+		{
+			return Equals(obj as PersonChange);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				var hashCode = ChangeId.GetHashCode();
+				hashCode = (hashCode * 397) ^ ChangeSystemId.GetHashCode();
+				hashCode = (hashCode * 397) ^ (Person != null ? Person.GetHashCode() : 0);
+				return hashCode;
+			}
+		}
+	}
+
+	class Person
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH1265/Fixture.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH1265
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				var p = new Person
+				{
+					Name = "Bob"
+				};
+				session.Save(p);
+				session.Save(
+					new PersonChange
+					{
+						Person = p
+					});
+
+				transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from PersonChange").ExecuteUpdate();
+				session.CreateQuery("delete from Person").ExecuteUpdate();
+
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void DoNothing()
+		{
+			
+		}
+	}
+}
