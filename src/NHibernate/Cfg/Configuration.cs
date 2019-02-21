dbfddd73ee669d8f5099ecfc8dca927a3451cff6
Alexander Zaytsev <hazzik@gmail.com>
2019-02-21 13:09:19 +1300

    Remove Dialect property from BindMappingEventArgs

--- src/NHibernate/Cfg/Configuration.cs
+++ src/NHibernate/Cfg/Configuration.cs
@@ -552,11 +552,11 @@ namespace NHibernate.Cfg
 			try
 			{
 				var dialect = new Lazy<Dialect.Dialect>(() => Dialect.Dialect.GetDialect(properties));
-				OnBeforeBindMapping(new BindMappingEventArgs(mappingDocument, documentFileName) {LazyDialect = dialect});
+				OnBeforeBindMapping(new BindMappingEventArgs(mappingDocument, documentFileName));
 				var mappings = CreateMappings();
 				mappings.LazyDialect = dialect;
 				new MappingRootBinder(mappings).Bind(mappingDocument);
-				OnAfterBindMapping(new BindMappingEventArgs(mappingDocument, documentFileName) {LazyDialect = dialect});
+				OnAfterBindMapping(new BindMappingEventArgs(mappingDocument, documentFileName));
 			}
 			catch (Exception e)
 			{
