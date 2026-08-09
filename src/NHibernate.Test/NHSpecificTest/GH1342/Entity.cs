d9c094b2fcccff6efbfb5f1b0f944a5dfbfebd46
Alex Zaytsev <hazzik@gmail.com>
2026-08-10 09:35:24 +1000

    NH-3851 - using Skip(aNumber).Count() where aNumber is greater than total count of records results in ArgumentOutOfRangeException

---
+++ src/NHibernate.Test/NHSpecificTest/GH1342/Entity.cs
@@ -0,0 +1,8 @@
+namespace NHibernate.Test.NHSpecificTest.GH1342
+{
+	public class Entity
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH1342/FixtureByCode.cs
@@ -0,0 +1,59 @@
+using System.Linq;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Linq;
+using NHibernate.Mapping.ByCode;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH1342
+{
+	[TestFixture]
+	public class FixtureByCode : TestCaseMappingByCode
+	{
+		protected override HbmMapping GetMappings()
+		{
+			var mapper = new ModelMapper();
+
+			mapper.Class<Entity>(
+				rc =>
+				{
+					rc.Id(x => x.Id, m => m.Generator(Generators.Identity));
+					rc.Property(x => x.Name);
+				});
+
+			return mapper.CompileMappingForAllExplicitlyAddedEntities();
+		}
+
+		protected override void OnSetUp()
+		{
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
+
+			session.Save(new Entity { Name = "Entity1" });
+			session.Save(new Entity { Name = "Entity2" });
+			session.Save(new Entity { Name = "Entity3" });
+
+			transaction.Commit();
+		}
+
+		protected override void OnTearDown()
+		{
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
+
+			session.CreateQuery("delete from Entity").ExecuteUpdate();
+
+			transaction.Commit();
+		}
+
+		[Test]
+		public void SkipWithinTotalCountThenCountReturnsRemainingCount()
+		{
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
+
+			var count = session.Query<Entity>().Skip(1).Count();
+
+			Assert.That(count, Is.EqualTo(2));
+		}
+	}
+}
