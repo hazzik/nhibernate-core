b7b31a423d1c082efc31fdc2dcb0eb6301c838cb
Alex Zaytsev <hazzik@gmail.com>
2026-08-10 09:45:11 +1000

    NH-3295 - Linq First or FirstOrDefault followed by Fetch return only one element in child collection of entity

---
+++ src/NHibernate.Test/NHSpecificTest/GH1141/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace NHibernate.Test.NHSpecificTest.GH1141
+{
+	public class Purchase
+	{
+		public virtual int Id { get; set; }
+		public virtual string Number { get; set; }
+		public virtual IList<Item> Items { get; set; } = new List<Item>();
+	}
+
+	public class Item
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH1141/FixtureByCode.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Linq;
+using NHibernate.Mapping.ByCode;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH1141
+{
+	[TestFixture]
+	public class FixtureByCode : TestCaseMappingByCode
+	{
+		protected override HbmMapping GetMappings()
+		{
+			var mapper = new ModelMapper();
+
+			mapper.Class<Purchase>(
+				rc =>
+				{
+					rc.Id(x => x.Id, m => m.Generator(Generators.Identity));
+					rc.Property(x => x.Number);
+					rc.Bag(
+						x => x.Items,
+						m => m.Cascade(Mapping.ByCode.Cascade.All),
+						r => r.OneToMany());
+				});
+
+			mapper.Class<Item>(
+				rc =>
+				{
+					rc.Id(x => x.Id, m => m.Generator(Generators.Identity));
+					rc.Property(x => x.Name);
+				});
+
+			return mapper.CompileMappingForAllExplicitlyAddedEntities();
+		}
+
+		protected override void OnSetUp()
+		{
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
+
+			var order = new Purchase { Number = "123" };
+			order.Items.Add(new Item { Name = "Item1" });
+			order.Items.Add(new Item { Name = "Item2" });
+			order.Items.Add(new Item { Name = "Item3" });
+			session.Save(order);
+
+			transaction.Commit();
+		}
+
+		protected override void OnTearDown()
+		{
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
+
+			session.CreateQuery("delete from Item").ExecuteUpdate();
+			session.CreateQuery("delete from Purchase").ExecuteUpdate();
+
+			transaction.Commit();
+		}
+
+		[Test]
+		public void FirstOrDefaultWithFetchReturnsAllChildren()
+		{
+			using var session = OpenSession();
+			using var transaction = session.BeginTransaction();
+
+			var order = session
+				.Query<Purchase>()
+				.Where(o => o.Number == "123")
+				.Fetch(o => o.Items)
+				.FirstOrDefault();
+
+			Assert.That(order, Is.Not.Null);
+			Assert.That(order.Items.Count, Is.EqualTo(3), "FirstOrDefault combined with Fetch on a collection must not truncate the fetched collection");
+
+			transaction.Commit();
+		}
+	}
+}
